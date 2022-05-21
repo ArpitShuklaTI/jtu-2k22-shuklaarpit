9dabf615f05d3fc2b477ab0b63f658b70582c030
arpit <arpitshukla62@gmail.com>
2022-05-21 08:22:09 +0530

    Separate out constants in a new utility file

--- restapi/models.py
+++ restapi/models.py
@@ -6,19 +6,22 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 
+from utils.contants import CATEGORY_NAME_MAX_LENGTH, GROUP_NAME_MAX_LENGTH, EXPENSE_DESCRIPTION_MAX_LENGTH, \
+    EXPENSE_AMOUNT_MAX_DIGITS, EXPENSE_AMOUNT_DECIMAL_PLACES
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, null=False)
+    name = models.CharField(max_length=CATEGORY_NAME_MAX_LENGTH, null=False)
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=GROUP_NAME_MAX_LENGTH, null=False)
     members = models.ManyToManyField(User, related_name='members', blank=True)
 
 
 class Expense(models.Model):
-    description = models.CharField(max_length=200)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=EXPENSE_DESCRIPTION_MAX_LENGTH)
+    total_amount = models.DecimalField(max_digits=EXPENSE_AMOUNT_MAX_DIGITS, decimal_places=EXPENSE_AMOUNT_DECIMAL_PLACES)
     group = models.ForeignKey(Group, null=True, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
 
@@ -26,8 +29,8 @@ class Expense(models.Model):
 class UserExpense(models.Model):
     expense = models.ForeignKey(Expense, default=1, on_delete=models.CASCADE, related_name="users")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expenses")
-    amount_owed = models.DecimalField(max_digits=10, decimal_places=2)
-    amount_lent = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_owed = models.DecimalField(max_digits=EXPENSE_AMOUNT_MAX_DIGITS, decimal_places=EXPENSE_AMOUNT_DECIMAL_PLACES)
+    amount_lent = models.DecimalField(max_digits=EXPENSE_AMOUNT_MAX_DIGITS, decimal_places=EXPENSE_AMOUNT_DECIMAL_PLACES)
 
     def __str__(self):
         return f"user: {self.user}, amount_owed: {self.amount_owed} amount_lent: {self.amount_lent}"
